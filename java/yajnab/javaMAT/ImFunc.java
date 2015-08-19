695485b3cb5ec04ddcbd9706e1dafdef90f3195d
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2015-08-20 00:38:07 +0530

    ImFunc.java : Add support for grayscale manipulation
    In many cases to deal with image processing grayscale image processing gives us the best result. This function gives an matrix output which is the final grayscale result.
    The function is written in ImReadGrayScale(File File, int choice)
    Here the Integer choice gives the following usage
    choice = 1
    	Gives a grayscale image output as grayScale.jpg
    choice !=1
    	Gives no image output. Directly a matrix of grayscale is generated

--- java/yajnab/javaMAT/ImFunc.java
+++ java/yajnab/javaMAT/ImFunc.java
@@ -7,11 +7,18 @@ package java.yajnab.javaMAT;
  */
 
 
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ColorConvertOp;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import static javax.swing.Spring.height;
+import static javax.swing.Spring.width;
 
 /**
  *
@@ -33,7 +40,7 @@ public class ImFunc {
   }
   }
   public static void ImReadSplitMatrix(File file)throws IOException {
-   BufferedImage image = ImageIO.read(file);  
+  BufferedImage image = ImageIO.read(file);  
   FileWriter redf = new FileWriter("red.txt",true);
   FileWriter greenf = new FileWriter("green.txt",true);
   FileWriter bluef = new FileWriter("blue.txt",true);
@@ -45,12 +52,37 @@ public class ImFunc {
             int  red   = (clr & 0x00ff0000) >> 16;
             int  green = (clr & 0x0000ff00) >> 8;
             int  blue  =  clr & 0x000000ff;
-            redf.write(red+"   ");
-            greenf.write(green+"   ");
-            bluef.write(blue+"    ");
+            redf.write(red+"\t");
+            greenf.write(green+"\t");
+            bluef.write(blue+"\t");
       }
   }
-  }   
   }
+  public static void ImReadGrayScale(File file, int choice)throws IOException {
+      BufferedImage image = null, imageo= null;
+       FileWriter matrix = new FileWriter("colormatrix.txt",true);
+      try{          
+      image = ImageIO.read(file);     
+      BufferedImageOp op = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null); 
+      imageo = op.filter(image, null);
+      if(choice==1){ImageIO.write(imageo,"jpeg",new File("dest.jpg"));}
+      }
+      catch (IOException ex) {
+            
+      }
+      if(imageo != null){
+      int x = imageo.getHeight();
+      int y = imageo.getWidth();
+      for (int i=0;i<y;i++){
+      for(int j=0;j<x;j++){          
+            int clr=  imageo.getRGB(i,j);
+            matrix.write(clr+"   ");
+            }
+  }}
+      
+      
+  }
+}
+
     
 
